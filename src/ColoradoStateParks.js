593234c53b5ae2c2d1a1e746b35623c9c2133ad6
NabiL Ashbat <nbl.dev21@gmail.com>
2023-02-11 23:19:38 +0300

    Completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,31 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import aDifferentName from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+// import { trees, wildlife } from "./parks/RockyMountain";
+import * as RMFunctions from "./parks/RockyMountain";
+
+import {
+    trees as parkTrees,
+    wildlife as parkWildlife,
+} from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+    // howManyParks(); // => "42 parks!"
+    // console.log(trees);
+    // console.log(RMFunctions.trees);
+    console.log(parkTrees);
+    return (
+        <div>
+            <MesaVerde/>
+        </div>
+    );
+    // wildlife();
+    // RMFunctions.wildlife();
+    parkWildlife();
+    aDifferentName(); // => "42 parks!"
+    return <h1>Colorado State Parks!</h1>;
+    // RMFunctions.elevation();
 }
+
+export default ColoradoStateParks;
